d2000921675417b4c3a0dbc871d61f108013545a
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-09-26 00:21:13 +0900

    [DOCS]: dto 스웨거 명세 추가

--- src/main/java/depth/mju/festival/domain/item/dto/response/ItemRes.java
+++ src/main/java/depth/mju/festival/domain/item/dto/response/ItemRes.java
@@ -1,5 +1,6 @@
 package depth.mju.festival.domain.item.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,10 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ItemRes {
 
+    @Schema(type = "Long", example = "1", description = "분실물의 id입니다.")
     private Long id;
 
+    @Schema(type = "String", example = "이어폰", description = "분실물 게시글의 제목입니다.")
     private String title;
 
+    @Schema(type = "String", example = "https://mju-festival-bucket.s3.amazonaws.com/014edc6a-48d0-4f11-8379-a4e48ba61402.jpg", description= "분실물 이미지 URL입니다.")
     private String imageUrl;
 
 
