ecf41a2d4baa075677c1ef1f6a402ab15d1f8799
jisu <98259433+jisujeong0@users.noreply.github.com>
2024-09-25 17:21:29 +0900

    initial commit

---
+++ src/test/java/depth/mju/festival/FestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package depth.mju.festival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
